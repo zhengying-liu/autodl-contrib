6195ef350214d009d5fe44e507cefaca154a6f1c
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-09-25 18:40:04 +0200

    Delete starting kit and use the original starting kit

--- utils/dataset_test/test_autodl_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Zhengying LIU
-# Creation date: 20 Sep 2018
-# Description: for a give AutoDL dataset, test it with baseline method in
-#   the competition bundle
-"""Run `bash test_autodl_dataset.sh <path_to_autodl_dataset>` to test the AutoDL
-dataset.
-"""
-# TODO
