1fcb7afa571d20398701cd29dba0b645bec9b413
Zhengying Liu <zhengying.liu@polytechnique.edu>
2020-01-20 13:39:31 +0100

    Add training time hook and VisualizeGraph hook

--- utils/tf_utils.py
+++ utils/tf_utils.py
@@ -18,11 +18,12 @@ def strip_consts(graph_def, max_const_size=32):
             tensor = n.attr['value'].tensor
             size = len(tensor.tensor_content)
             if size > max_const_size:
-                tensor.tensor_content = "<stripped %d bytes>"%size
+                tensor.tensor_content = "<stripped {} bytes>".format(size).encode()
     return strip_def
 
-def show_graph(graph_def, max_const_size=32):
-    """Visualize TensorFlow graph."""
+
+def to_html(graph_def, max_const_size=32, width=1000, height=620):
+    """Convert TensorBoard summary to HTML."""
     if hasattr(graph_def, 'as_graph_def'):
         graph_def = graph_def.as_graph_def()
     strip_def = strip_consts(graph_def, max_const_size=max_const_size)
@@ -39,10 +40,38 @@ def show_graph(graph_def, max_const_size=32):
     """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
 
     iframe = """
-        <iframe seamless style="width:1000px;height:620px;border:0" srcdoc="{}"></iframe>
-    """.format(code.replace('"', '&quot;'))
+        <iframe seamless style="width:{}px;height:{}px;border:0" srcdoc="{}"></iframe>
+    """.format(width, height, code.replace('"', '&quot;'))
+    return iframe
+
+
+def show_graph(graph_def, max_const_size=32, width=1000, height=620):
+    """Visualize TensorFlow graph."""
+    iframe = to_html(graph_def, max_const_size=max_const_size,
+                     width=width, height=height)
     display(HTML(iframe))
 
-def show_default_graph():
+
+def show_default_graph(width=1000, height=620):
     graph_def = tf.get_default_graph().as_graph_def()
-    show_graph(graph_def)
+    show_graph(graph_def, width=width, height=height)
+
+
+def save_graph_to_html(filename, width=1200, height=620):
+    """Save HTML file corresponding to current TensorFlow file."""
+    iframe = to_html(tf.get_default_graph().as_graph_def(),
+                     width=width, height=height)
+    if not filename.endswith('.html'):
+        filename += '.html'
+    with open(filename, 'w') as f:
+        f.write(iframe)
+
+
+# End block for visualization
+class VisulizeGraphHook(tf.train.SessionRunHook):
+
+  def __init__(self, html_path):
+    self.html_path = html_path
+
+  def begin(self):
+    save_graph_to_html(self.html_path)
